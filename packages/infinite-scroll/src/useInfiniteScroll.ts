c388ff544afb94809e877ebbb9dcc14d23c931a8
Jungyu Park <parkjungyu09@gmail.com>
2025-11-23 20:29:14 +0900

    feat: useInfiniteScroll 기본 구조 작성

--- packages/infinite-scroll/src/useInfiniteScroll.ts
+++ packages/infinite-scroll/src/useInfiniteScroll.ts
@@ -1,1 +1,31 @@
-// TODO: useInfiniteScroll 훅 구현
+import { useRef } from "react";
+
+/**
+ * 무한 스크롤을 위한 옵션 타입
+ */
+export interface UseInfiniteScrollOptions {
+  // TODO: 옵션 타입 정의
+}
+
+/**
+ * useInfiniteScroll 훅의 반환 타입
+ */
+export interface UseInfiniteScrollReturn {
+  // TODO: 반환 타입 정의
+}
+
+/**
+ * React hook for infinite scroll functionality
+ *
+ * @param options - 무한 스크롤 옵션
+ * @returns 무한 스크롤 관련 ref와 함수들
+ */
+export function useInfiniteScroll(
+  options: UseInfiniteScrollOptions
+): UseInfiniteScrollReturn {
+  // TODO: 로직 구현
+
+  return {
+    // TODO: 반환값 구현
+  };
+}
