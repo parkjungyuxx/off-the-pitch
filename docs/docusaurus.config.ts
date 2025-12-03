1bf60bdbf0ea9d69384e21a9ea8c32b901858e1b
Jungyu Park <parkjungyu09@gmail.com>
2025-12-04 01:08:48 +0900

    docs: base url 변경

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -15,10 +15,10 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://parkjungyuxx.github.io',
+  url: 'https://bongsik-libraries.vercel.app',
   // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/bongsik/',
+  // For standalone deployment, use '/'
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
